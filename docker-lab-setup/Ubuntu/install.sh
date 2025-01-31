962f0464665417b57d585409e504a3f5b8200725
Kanaestephe <kanaestephe@gmail.com>
2025-01-31 20:11:25 +0100

    install AWS-CLI

--- docker-lab-setup/Ubuntu/install.sh
+++ docker-lab-setup/Ubuntu/install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 sudo apt-get update -y
+
+# install AWS-CLI
+sudo apt install curl unzip -y
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+# Install docker
 sudo apt-get install -y docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
